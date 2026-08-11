6dc09c350545021dcce3d6af9a06c1698831da16
TaxiDriverCat <taxidrivercat@outlook.com>
2026-08-10 21:00:57 -0400

    Create DB if it doesn't exist

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+const fs = require('fs').promises;
 const path = require('path');
 const config = require('../config');
 const WatermarkJob = require('./models/WatermarkJob');
@@ -6,11 +7,15 @@ const WatermarkJob = require('./models/WatermarkJob');
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: config.dbPath,
+  logging: false,
 });
 
 WatermarkJob.init(sequelize);
 
 async function initDatabase() {
+  const dbDir = path.dirname(config.dbPath);
+  await fs.mkdir(dbDir, { recursive: true });
+
   await sequelize.authenticate();
   await WatermarkJob.sync({ alter: true });
   return sequelize;
